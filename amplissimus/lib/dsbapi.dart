4ed5a341593f516036b777cee524acecde51112c
chrissx on Windows <chrissx@chrissx.de>
2020-05-26 16:17:03 +0200

    implemented returning the response body in DsbAccount.getData

--- amplissimus/lib/dsbapi.dart
+++ amplissimus/lib/dsbapi.dart
@@ -2,11 +2,10 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:uuid/uuid.dart';
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
-const String DSB_BUNDLE_ID = "de.heinekingmedia.inhouse.dsbmobile.web";
+const String DSB_BUNDLE_ID = "de.heinekingmedia.dsbmobile";
 const String DSB_DEVICE = "SM-G935F";
-const String DSB_ID = "";
 const String DSB_VERSION = "2.5.9";
 const String DSB_OS_VERSION = "28 8.0";
 const String DSB_LANGUAGE = "de";
@@ -30,12 +29,13 @@ class DsbAccount {
 
   DsbAccount(this.username, this.password);
 
-  void getData() {
+  String getData() async {
     String datetime = removeLastChars(DateTime.now().toIso8601String(), 3) + 'Z';
     String uuid = new Uuid().v4();
     String json = '{"UserId":"$username","UserPw":"$password","AppVersion":"$DSB_VERSION","Language":"$DSB_LANGUAGE","OsVersion":"$DSB_OS_VERSION","AppId":"$uuid","Device":"$DSB_DEVICE","BundleId":"$DSB_BUNDLE_ID","Date":"$datetime","LastUpdate":"$datetime"}';
     base64.encode(gzip.encode(utf8.encode(json)));
-    http.post(DSB_WEBSERVICE, body: '{"req": {"Data": "${base64.encode(gzip.encode(utf8.encode(json)))}", "DataType": 1}}');
+    Response res = await post(DSB_WEBSERVICE, body: '{"req": {"Data": "${base64.encode(gzip.encode(utf8.encode(json)))}", "DataType": 1}}');
+    return res.body;
   }
 }
 
