98fcce5fb74ab1df97a4f7415a7e86fdc04dfd20
chrissx on Windows <chrissx@chrissx.de>
2020-05-28 18:38:32 +0200

    improved the logging

--- amplissimus/lib/logging.dart
+++ amplissimus/lib/logging.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 
 void ampLog({@required String ctx, @required Object message}) {
   var now = DateTime.now();
-  var hour = now.hour;
-  var minute = now.minute;
-  var second = now.second;
-  print('[$hour:$minute:$second - $ctx] $message');
+  print('[${now.hour}:${now.minute}:${now.second}][$ctx] $message');
+}
+
+void ampErr({@required String ctx, @required Object message}) {
+  ampLog(ctx: 'Error][' + ctx, message: message);
+}
+
+void ampWarn({@required String ctx, @required Object message}) {
+  ampLog(ctx: 'Warning][' + ctx, message: message);
+}
+
+void ampInfo({@required String ctx, @required Object message}) {
+  ampLog(ctx: 'Info][' + ctx, message: message);
 }
