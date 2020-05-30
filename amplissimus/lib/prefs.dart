3b89349cf34ed3ddae0b6ef2328d81f718b8cbb4
chrissx on Windows <chrissx@chrissx.de>
2020-05-30 14:17:40 +0200

    fixed the prefs by waiting for the SharedPreferences to load

--- amplissimus/lib/prefs.dart
+++ amplissimus/lib/prefs.dart
@@ -1,50 +1,67 @@
+import 'dart:io';
+
 import 'package:amplissimus/logging.dart';
 import 'package:amplissimus/values.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 SharedPreferences preferences;
 
+void waitForPrefs() {
+  while(preferences == null) sleep(Duration(milliseconds: 10));
+}
+
 int get counter {
+  waitForPrefs();
   int i = preferences.getInt('counter');
   if(i == null) i = 0;
   return i;
 }
 set counter(int i) {
+  waitForPrefs();
   preferences.setInt('counter', i);
 }
 String get username {
+  waitForPrefs();
   String s = preferences.getString('username_dsb');
   if(s == null) s = '';
   return s;
 }
 set username(String s) {
+  waitForPrefs();
   preferences.setString('username_dsb', s);
 }
 String get password {
+  waitForPrefs();
   String s = preferences.getString('password_dsb');
   if(s == null) s = '';
   return s;
 }
 set password(String s) {
+  waitForPrefs();
   preferences.setString('password_dsb', s);
 }
 String get grade {
+  waitForPrefs();
   String s = preferences.getString('grade');
   if(s == null) s = '';
   return s;
 }
 set grade(String s) {
+  waitForPrefs();
   preferences.setString('grade', s);
 }
 String get char {
+  waitForPrefs();
   String s = preferences.getString('char');
   if(s == null) s = '';
   return s;
 }
 set char(String s) {
+  waitForPrefs();
   preferences.setString('char', s);
 }
 set designMode(bool isDarkMode) {
+  waitForPrefs();
   preferences.setBool('is_dark_mode', isDarkMode);
 }
 
@@ -57,6 +74,7 @@ void loadPrefs() async {
 }
 
 void clear() {
+  waitForPrefs();
   preferences.clear();
   ampInfo(ctx: 'Prefs', message: 'Cleared SharedPreferences.');
 }
