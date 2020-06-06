719cf9e4e2ac85ec6b181721f25541c7e6250adb
chrissx on Windows <chrissx@chrissx.de>
2020-06-06 17:04:04 +0200

    added a default value to the prefs

--- amplissimus/lib/prefs.dart
+++ amplissimus/lib/prefs.dart
@@ -15,38 +15,38 @@ void set(String key, dynamic value) {
   else throw '${value.runtimeType} cannot be saved in the SharedPreferences.';
 }
 
-int getInt(String key) {
-  if(preferences == null) throw '';
+int getInt(String key, int defaultValue) {
+  if(preferences == null) throw 'PREFSI NOT INITIALIZED, THIS IS A SEVERE CODE BUG';
   int i = preferences.getInt(key);
   if(i == null) i = 0;
   return i;
 }
 
-String getString(String key) {
-  if(preferences == null) throw '';
+String getString(String key, String defaultValue) {
+  if(preferences == null) throw 'PREFSS NOT INITIALIZED, THIS IS A SEVERE CODE BUG';
   String s = preferences.getString(key);
   if(s == null) s = '';
   return s;
 }
 
-bool getBool(String key) {
-  if(preferences == null) throw '';
+bool getBool(String key, bool defaultValue) {
+  if(preferences == null) throw 'PREFSB NOT INITIALIZED, THIS IS A SEVERE CODE BUG';
   bool b = preferences.getBool(key);
   if(b == null) b = false;
   return b;
 } 
 
-int get counter => getInt('counter');
+int get counter => getInt('counter', 0);
 set counter(int i) => set('counter', i);
-String get username => getString('username_dsb');
+String get username => getString('username_dsb', '');
 set username(String s) => set('username_dsb', s);
-String get password => getString('password_dsb');
+String get password => getString('password_dsb', '');
 set password(String s) => set('password_dsb', s);
-String get grade => getString('grade').toLowerCase();
+String get grade => getString('grade', '').toLowerCase();
 set grade(String s) => set('grade', s.toLowerCase());
-String get char => getString('char').toLowerCase();
+String get char => getString('char', '').toLowerCase();
 set char(String s) => set('char', s.toLowerCase());
-bool get loadingBarEnabled => getBool('loading_bar_enabled');
+bool get loadingBarEnabled => getBool('loading_bar_enabled', false);
 set loadingBarEnabled(bool b) => set('loading_bar_enabled', b);
 
 set designMode(bool isDarkMode) => set('is_dark_mode', isDarkMode);
