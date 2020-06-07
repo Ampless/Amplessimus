5af67bbe9f597bc15249fce5d2e0257380b10c26
Luddifee <info@ludwig-lehnert.de>
2020-06-07 19:21:59 +0200

    general update

--- amplissimus/lib/dsbapi.dart
+++ amplissimus/lib/dsbapi.dart
@@ -329,6 +329,7 @@ Widget dsbGetGoodList(List<DsbPlan> plans) {
       },)
     ])));
     widgets.add(Card(
+      elevation: 0,
       color: AmpColors.lightForeground,
       child: Column(mainAxisSize: MainAxisSize.min, children: dayWidgets),
     ));
