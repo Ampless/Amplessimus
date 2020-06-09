8a28f78a7570b25d02c1c54bd5521a549320ec14
chrissx <christian.chrissicx2004@freenet.de>
2020-06-09 23:37:17 +0200

    basically fixed the whole build system, reducing the ipa, deb and apk size DRASTICALLY (only like 1-3 megs for the apk, but ipa shrank from 100M, reduced from 180k because of the last commit, to about 20k and deb is about the same

--- amplissimus/build_deb.bash
+++ amplissimus/build_deb.bash
@@ -7,7 +7,6 @@ set -u
 DEB_BUILD_DIR=build/tmp
 DEB_METADATA_DIR=$DEB_BUILD_DIR/DEBIAN
 DEB_APP_DIR=$DEB_BUILD_DIR/Applications
-DEB_DST=build/$1.deb
 
 APPLICATION_DIR=build/ios/iphoneos/Runner.app
 APPLICATION_SIZE=`du -sk $APPLICATION_DIR | awk '{ print $1 }'`
@@ -24,7 +23,7 @@ cp -rp $APPLICATION_DIR $DEB_APP_DIR/
 export COPYFILE_DISABLE
 export COPY_EXTENDED_ATTRIBUTES_DISABLE
 
-dpkg-deb -Sextreme -z9 --build $DEB_BUILD_DIR $DEB_DST
+dpkg-deb -Sextreme -z9 --build $DEB_BUILD_DIR $1.deb
 
 rm -rf $DEB_BUILD_DIR
 exit 0
