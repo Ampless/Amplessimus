914abe393f5c7e7e898b472005c01f3a7d175788
chrissx on Windows <chrissx@chrissx.de>
2020-06-27 18:23:19 +0200

    reimplemented the date and commitid to not depend on ci4w1nner and allow for manually rebuilding

--- ci.sh
+++ ci.sh
@@ -6,8 +6,11 @@ mkdir -p /usr/local/var/www/amplissimus
 cd amplissimus
 #flutter pub cache repair # this might fix some stupid problems with shared_preferences_macos
 make ci || { make cleanartifacts rollbackversions ; exit 1 ; }
-cp -rf bin "/usr/local/var/www/amplissimus/$1"
+commitid=$(git rev-parse @)
+date=$(date +%Y_%m_%d-%H_%M_%S)
+output_dir=$date-$commitid
+cp -rf bin "/usr/local/var/www/amplissimus/$output_dir"
 cd bin
-tar cf "/usr/local/var/www/amplissimus/$1/$2.tar" *
+tar cf "/usr/local/var/www/amplissimus/$output_dir/$commitid.tar" *
 rm -rf *
 cd ../..
