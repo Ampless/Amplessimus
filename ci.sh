ffe8d5bc0a9b18c177edb08246e9a6018dc95c77
chrissx <chrissx@chrissx.de>
2020-06-28 03:54:57 +0200

    added a way for the ci to automatically create release drafts

--- ci.sh
+++ ci.sh
@@ -1,4 +1,36 @@
 #!/bin/sh
+
+# create a github release
+# args:
+#  commitid (1)
+#  name     (2)
+gh_create_release() {
+        curl -X POST -u "$(cat /etc/ampci.creds)" \
+                -H "Accept: application/vnd.github.v3+json" \
+                --data "{
+                         \"tag_name\": \"$1\",
+                         \"target_commitish\": \"$1\",
+                         \"name\": \"$2\",
+                         \"body\": \"This is an automatic release by the ci.\\n\\n###### Changelog\\n\\n\\n###### Related Issues\\n\\n\\n###### Known Bugs\\n\",
+                         \"draft\": true,
+                         \"prerelease\": true
+                        }" \
+                https://api.github.com/repos/Amplissimus/Amplissimus/releases \
+                | grep '"upload_url":' | head -n 1 | cut -d: -f2- | sed 's/ \"\(.\+\)\",/\1/'
+}
+
+# upload a file to a github release
+# args:
+#  output of gh_create_release (1)
+#  file                        (2)
+gh_upload_binary() {
+        curl -X POST -u "$(cat /etc/ampci.creds)" \
+                -H "Accept: application/vnd.github.v3+json" \
+                -H "Content-Type: application/octet-stream" \
+                --data-binary "@$2" \
+                "$(echo "$1" | sed "s/{?name,label}/?name=$2/")"
+}
+
 flutter channel master
 flutter upgrade
 flutter config --enable-web --enable-windows-desktop --enable-macos-desktop --enable-linux-desktop
@@ -8,9 +40,16 @@ cd amplissimus
 make ci || { make cleanartifacts rollbackversions ; exit 1 ; }
 commitid=$(git rev-parse @)
 date=$(date +%Y_%m_%d-%H_%M_%S)
-output_dir=$date-$commitid
-cp -rf bin "/usr/local/var/www/amplissimus/$output_dir"
+version_name=$date-$commitid
+output_dir="/usr/local/var/www/amplissimus/$version_name"
+cp -rf bin "$output_dir"
 cd bin
-tar cf "/usr/local/var/www/amplissimus/$output_dir/$commitid.tar" *
+tar cf "$output_dir/$commitid.tar" *
 rm -rf *
 cd ../..
+[ ! -f /etc/ampci.creds ] && { echo "No GitHub creds found." ; exit 1 ; }
+cd $output_dir
+upload_url=$(gh_create_release $commitid $version_name)
+for fn in * ; do
+        gh_upload_binary $upload_url $fn
+done
