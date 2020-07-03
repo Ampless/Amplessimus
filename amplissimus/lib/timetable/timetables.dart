8fc3b1e3802493e0738f9ce7dffd16e0f2dbf35d
chrissx on Windows <chrissx@chrissx.de>
2020-07-03 21:42:54 +0200

    fixed a denkfehler in timetables

--- amplissimus/lib/timetable/timetables.dart
+++ amplissimus/lib/timetable/timetables.dart
@@ -15,7 +15,7 @@ enum TTDay {
 }
 
 TTDay ttDayFromInt(int i) {
-  if(i == null) return TTDay.Null;
+  if (i == null) return TTDay.Null;
   switch (i) {
     case 0:
       return TTDay.Monday;
@@ -35,7 +35,7 @@ TTDay ttDayFromInt(int i) {
 }
 
 int ttDayToInt(TTDay day) {
-  if(day == null) return -1;
+  if (day == null) return -1;
   switch (day) {
     case TTDay.Monday:
       return 0;
@@ -119,19 +119,6 @@ class TTColumn {
   }
 }
 
-TTColumn ttSubColumn(TTColumn column, List<DsbSubstitution> subs) {
-  for (int i = 0; i < column.lessons.length; i++) {
-    for (DsbSubstitution sub in subs) {
-      if (sub.actualHours.contains(i)) {
-        column.lessons[i].teacher = sub.teacher;
-        column.lessons[i].notes = sub.notes;
-        column.lessons[i].isFree = sub.isFree;
-      }
-    }
-  }
-  return column;
-}
-
 const List<TTDay> ttWeek = [
   TTDay.Monday,
   TTDay.Tuesday,
@@ -163,7 +150,22 @@ List<TTColumn> ttSubTable(List<TTColumn> table, List<DsbPlan> plans) {
   for (DsbPlan plan in plans) {
     for (int i = 0; i < table.length; i++) {
       if (table[i].day == plan.day) {
-        table[i] = ttSubColumn(table[i], plan.subs);
+        TTColumn column = table[i];
+        for (int i = 0; i < column.lessons.length; i++) {
+          for (DsbSubstitution sub in plan.subs) {
+            if (sub.actualHours.contains(i) &&
+                (sub.subject
+                        .toLowerCase()
+                        .contains(column.lessons[i].subject.toLowerCase()) ||
+                    column.lessons[i].subject
+                        .toLowerCase()
+                        .contains(sub.subject.toLowerCase()))) {
+              column.lessons[i].teacher = sub.teacher;
+              column.lessons[i].notes = sub.notes;
+              column.lessons[i].isFree = sub.isFree;
+            }
+          }
+        }
       }
     }
   }
