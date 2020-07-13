94fc281d8e55eb24ebc8c4ecad98c4a94f7d074d
chrissx on Windows <chrissx@chrissx.de>
2020-07-13 22:07:46 +0200

    flutter has some new windows stuff

--- amplissimus/windows/runner/window_configuration.cpp
+++ amplissimus/windows/runner/window_configuration.cpp
@@ -4,4 +4,4 @@ const wchar_t* kFlutterWindowTitle = L"Amplissimus";
 const unsigned int kFlutterWindowOriginX = 10;
 const unsigned int kFlutterWindowOriginY = 10;
 const unsigned int kFlutterWindowWidth = 400;
-const unsigned int kFlutterWindowHeight = 700;
+const unsigned int kFlutterWindowHeight = 800;
