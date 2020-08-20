c33466e95deec93b88456fadab16d1d75e97bfc5
Chris (@ Windows) <chrissx@chrissx.de>
2020-08-20 02:41:19 +0200

    flutter create . (windows update, actually seems to break)

--- windows/runner/window_configuration.cpp
+++ windows/runner/window_configuration.cpp
@@ -3,5 +3,5 @@
 const wchar_t* kFlutterWindowTitle = L"Amplessimus";
 const unsigned int kFlutterWindowOriginX = 10;
 const unsigned int kFlutterWindowOriginY = 10;
-const unsigned int kFlutterWindowWidth = 414;
-const unsigned int kFlutterWindowHeight = 896;
+const unsigned int kFlutterWindowWidth = 450;
+const unsigned int kFlutterWindowHeight = 900;
