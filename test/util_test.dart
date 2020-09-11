ae966f16cca3d587729dbc6dca91144a99a0dc80
Chris <chrissx@chrissx.de>
2020-09-11 23:20:17 +0200

    schttp 0.2.0

--- test/util_test.dart
+++ test/util_test.dart
@@ -3,56 +3,7 @@ import 'package:Amplessimus/dsbhtmlcodes.dart' as htmlcodes;
 
 import 'testlib.dart';
 
-enum HttpMethod {
-  GET,
-  POST,
-}
-
-testCase httpTestCase(String url, HttpMethod method, Object body,
-        Map<String, String> headers) =>
-    () async {
-      var _setCacheCalled = false;
-      var _getCacheCalled = false;
-      if (method == HttpMethod.GET)
-        await httpGet(
-          Uri.parse(url),
-          setCache: (_, __, ___) => _setCacheCalled = true,
-          getCache: (_) {
-            _getCacheCalled = true;
-            return null;
-          },
-          flushCache: null,
-        );
-      else if (method == HttpMethod.POST)
-        await httpPost(
-          Uri.parse(url),
-          body,
-          null,
-          headers,
-          setCache: (_, __, ___) => _setCacheCalled = true,
-          getCache: (_) {
-            _getCacheCalled = true;
-            return null;
-          },
-          flushCache: null,
-        );
-      else
-        throw 'The test is broken.';
-      assert(_setCacheCalled && _getCacheCalled);
-    };
-
-testCase getCase(String url) => httpTestCase(url, HttpMethod.GET, null, null);
-
-testCase postCase(String url, Object body, Map<String, String> headers) =>
-    httpTestCase(url, HttpMethod.POST, body, headers);
-
-List<testCase> httpTestCases = [
-  getCase('https://example.com/'),
-  postCase('https://example.com/', 'this is a test', {}),
-];
-
 void main() {
-  tests(httpTestCases, 'dsbutil http');
   tests([
     () async {
       var keys = '&lulwdisisnocode;&#9773;';
