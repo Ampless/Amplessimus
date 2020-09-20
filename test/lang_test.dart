cdb0db186aeb4de59752020a18d77c06d0d7997c
Chris <chrissx@chrissx.de>
2020-09-20 22:58:57 +0200

    removed a few bad tests

--- test/lang_test.dart
+++ test/lang_test.dart
@@ -1,6 +1,5 @@
 import 'package:dsbuntis/dsbuntis.dart';
 import 'package:Amplessimus/langs/language.dart';
-import 'package:flutter_test/flutter_test.dart';
 
 import 'testlib.dart';
 
@@ -23,47 +22,18 @@ List<testCase> languageTestCases = [
     assert(Language.fromCode(lang.code) == lang);
   }),
   languageTestCase((lang) {
-    lang.dsbSubtoTitle(null);
-  }),
-  languageTestCase((lang) {
     lang.dsbSubtoSubtitle(null);
-  }),
-  languageTestCase((lang) {
-    lang.dsbSubtoTitle(DsbSubstitution(null, null, null, null, null, false));
   }),
   languageTestCase((lang) {
     lang.dsbSubtoSubtitle(DsbSubstitution(null, null, null, null, null, false));
   }),
   languageTestCase((lang) {
-    lang.dsbSubtoTitle(
-        DsbSubstitution('lul', [], 'kek', 'subJEeKE', 'notesnotes', false));
-  }),
-  languageTestCase((lang) {
     lang.dsbSubtoSubtitle(
         DsbSubstitution('lul', [], 'kek', 'subJEeKE', 'notesnotes', false));
-  }),
-  languageTestCase((lang) {
-    lang.dsbSubtoTitle(
-        DsbSubstitution('lul', [1, 3, 5], 'kek', '1sk1', 'not', false));
   }),
   languageTestCase((lang) {
     lang.dsbSubtoSubtitle(
         DsbSubstitution('lul', [1, 3, 5], '---', 'sub', 'not', true));
-  }),
-  languageTestCase((lang) {
-    if (lang == null || lang.code != 'de') return;
-    expect(lang.dsbSubtoTitle(DsbSubstitution('', [1], '', 'a', '', null)),
-        '1. Stunde a');
-  }),
-  languageTestCase((lang) {
-    if (lang == null || lang.code != 'en') return;
-    expect(lang.dsbSubtoTitle(DsbSubstitution('', [11], '', 'b', '', null)),
-        '11th lesson Biology');
-  }),
-  languageTestCase((lang) {
-    if (lang == null || lang.code != 'en') return;
-    expect(lang.dsbSubtoTitle(DsbSubstitution('', [23], '', 'b', '', null)),
-        '23rd lesson Biology');
   }),
 ];
 
