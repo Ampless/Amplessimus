8b0294c5d726bfc5167669328b2370d6906900f8
Chris <chrissx@chrissx.de>
2020-09-24 21:35:42 +0200

    actually html_unescape is a good package

--- lib/dsbutil.dart
+++ lib/dsbutil.dart
@@ -1,60 +1,12 @@
-// this code is based on the pub package 'html_unescape'
-
 import 'dart:math';
-import 'package:Amplessimus/dsbhtmlcodes.dart' as htmlcodes;
 import 'package:Amplessimus/prefs.dart' as Prefs;
+import 'package:html_unescape/html_unescape.dart';
 import 'package:schttp/schttp.dart';
 import 'dart:async';
 
-var rand = Random();
-
-String htmlUnescape(String data) {
-  //this optimization is kind of unnecessary for small strings
-  if (!data.contains('&')) return data;
-  var buf = StringBuffer();
-  var offset = 0;
-  while (true) {
-    var nextAmp = data.indexOf('&', offset);
-    if (nextAmp == -1) {
-      buf.write(data.substring(offset));
-      break;
-    }
-    buf.write(data.substring(offset, nextAmp));
-    offset = nextAmp;
-    var chunk = data.substring(offset, min(data.length, offset + 18));
-    if (chunk.length > 4 && chunk.codeUnitAt(1) == 35) {
-      var nextSemicolon = chunk.indexOf(';');
-      if (nextSemicolon != -1) {
-        var hex = chunk.codeUnitAt(2) == 120;
-        var str = chunk.substring(hex ? 3 : 2, nextSemicolon);
-        var ord = int.tryParse(str, radix: hex ? 16 : 10);
-        if (ord != null) {
-          buf.write(String.fromCharCode(ord));
-          offset += nextSemicolon + 1;
-          continue;
-        }
-      }
-    }
-    var replaced = false;
-    for (var i = 0; i < htmlcodes.keys.length; i++) {
-      var key = htmlcodes.keys[i];
-      if (chunk.startsWith(key)) {
-        var replacement = htmlcodes.values[i];
-        buf.write(replacement);
-        offset += key.length;
-        replaced = true;
-        break;
-      }
-    }
-    if (!replaced) {
-      buf.write('&');
-      offset++;
-    }
-  }
-  return buf.toString();
-}
-
-var _httpClient = ScHttpClient(Prefs.getCache, Prefs.setCache);
+final rand = Random();
+final http = ScHttpClient(Prefs.getCache, Prefs.setCache);
+final unescape = HtmlUnescape();
 
 Future<String> httpPost(
   Uri url,
@@ -62,10 +14,11 @@ Future<String> httpPost(
   String id,
   Map<String, String> headers,
 ) =>
-    _httpClient.post(url, body, id, headers);
+    http.post(url, body, id, headers);
 
 Future<String> httpGet(Uri url) async {
-  return htmlUnescape(await _httpClient.get(url))
+  return unescape
+      .convert(await http.get(url))
       .replaceAll('\n', '')
       .replaceAll('\r', '')
       //just fyi: these regexes only work because there are no more newlines
