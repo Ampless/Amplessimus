e92819dcbea15fa0f84c3ac68d91d2ddbec1fe03
Chris Häußler (@ Windows) <chrissx@chrissx.de>
2020-10-30 14:37:51 +0100

    Revert "removed flare, hopefully one of the best changes ever"
    This reverts commit f473d18c8c6b9c64d3f2d4a6bf67d42d4678707b.

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ import 'package:Amplessimus/timetables.dart';
 import 'package:Amplessimus/uilib.dart';
 import 'package:Amplessimus/values.dart';
 import 'package:dsbuntis/dsbuntis.dart';
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
@@ -71,6 +72,8 @@ class SplashScreenPageState extends State<SplashScreenPage> {
         () => ampChangeScreen(Timeout(), context),
       );
 
+      //final minimalLoadingTime = Future.delayed(Duration(milliseconds: 700));
+
       ampInfo('SplashScreen', 'Loading SharedPreferences...');
       await Prefs.load();
       ampInfo('SplashScreen', 'SharedPreferences successfully loaded.');
@@ -84,6 +87,8 @@ class SplashScreenPageState extends State<SplashScreenPage> {
 
       if (!Prefs.firstLogin) await dsbUpdateWidget();
 
+      //await minimalLoadingTime;
+
       timeout.cancel();
       ampChangeScreen(
           Prefs.firstLogin ? FirstLoginScreen() : AmpApp(), context);
@@ -93,7 +98,32 @@ class SplashScreenPageState extends State<SplashScreenPage> {
   }
 
   @override
-  Widget build(BuildContext context) => Scaffold(backgroundColor: Colors.black);
+  Widget build(BuildContext context) {
+    try {
+      ampInfo('SplashScreen', 'Buiding Splash Screen');
+      return Scaffold(
+        backgroundColor: Colors.black,
+        //body: Center(
+        //  child: AnimatedContainer(
+        //    color: Colors.black,
+        //    height: double.infinity,
+        //    width: double.infinity,
+        //    duration: Duration(seconds: 1),
+        //    child: FlareActor(
+        //      'assets/splash_screen.json',
+        //      alignment: Alignment.center,
+        //      fit: BoxFit.contain,
+        //      animation: 'anim',
+        //    ),
+        //  ),
+        //),
+        //bottomSheet: ampLinearProgressIndicator(),
+      );
+    } catch (e) {
+      ampErr('SplashScreenPageState', errorString(e));
+      return ampText(errorString(e));
+    }
+  }
 }
 
 class AmpApp extends StatelessWidget {
