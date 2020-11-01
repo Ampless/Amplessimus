e65fb72c6e7e1d6a3eb613ce9bc72b40db34cbab
Chris <chrissx@chrissx.de>
2020-11-01 10:59:01 +0100

    made the subject code bigger but cleaner

--- lib/subject.dart
+++ lib/subject.dart
@@ -1,10 +1,10 @@
 import 'package:Amplessimus/langs/language.dart';
 
 final _zero = '0'.codeUnitAt(0), _nine = '9'.codeUnitAt(0);
-final _letters = RegExp(r'[a-zA-Z]');
-final _numeric = RegExp(r'[0-9]');
+final _letters = RegExp('[a-zA-Z]');
+final _numbers = RegExp('[0-9]');
 
-bool _isNum(String s, int i) {
+bool _numAt(String s, int i) {
   if (s == null || s.length <= i || i < 0) return false;
   final cu = s.codeUnitAt(i);
   return cu >= _zero && cu <= _nine;
@@ -13,15 +13,23 @@ bool _isNum(String s, int i) {
 String realSubject(String subject, [Language lang]) {
   lang ??= Language.current;
   if (subject == null) return null;
-  //this code might break with some newer systems
-  if (_isNum(subject, 0) || _isNum(subject, subject.length - 1))
-    return '${realSubject(subject.substring(subject.indexOf(_letters), subject.lastIndexOf(_letters) + 1), lang)} '
-        '${subject.substring(subject.lastIndexOf(_numeric))}'
-        '${subject.indexOf(_letters) > 0 ? " (${subject.substring(0, subject.indexOf(_letters))})" : ""}';
+
+  //this code might break, but it hasnt as of nov 2020
+  if (_numAt(subject, 0) || _numAt(subject, subject.length - 1)) {
+    final firstLetter = subject.indexOf(_letters);
+    final shortSubject =
+        subject.substring(firstLetter, subject.lastIndexOf(_letters) + 1);
+    final lnum = subject.substring(subject.lastIndexOf(_numbers));
+    var fnum = '';
+    if (firstLetter > 0) fnum = ' (${subject.substring(0, firstLetter)})';
+    return '${realSubject(shortSubject, lang)} $lnum$fnum';
+  }
+
   final sub = subject.toLowerCase();
   var s = subject;
   final lut = lang.subjectLut;
   for (var key in lut.keys) if (sub.startsWith(key)) s = lut[key];
+
   return s;
 }
 
