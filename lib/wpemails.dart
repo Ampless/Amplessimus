a6133a9664cfed3d689e89d8e03b562d3871220b
Chris <chrissx@chrissx.de>
2020-11-01 10:59:22 +0100

    wpemails widgets

--- lib/wpemails.dart
+++ lib/wpemails.dart
@@ -1,26 +1,49 @@
 import 'package:Amplessimus/first_login.dart';
+import 'package:Amplessimus/uilib.dart';
+import 'package:flutter/material.dart';
 import 'package:html_search/html_search.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+Map<String, String> wpemailsave;
 
 Future<Map<String, String>> wpemails(String domain) async {
-  final result = <String, String>{};
-  var html = htmlParse(
-    await httpGetFunc(Uri.parse('https://$domain/schulfamilie/lehrkraefte/')),
-  );
-  html = htmlSearchByClass(html, 'entry-content').children;
-  html = htmlSearchAllByPredicate(
-      html,
-      (e) =>
-          e.innerHtml.contains(',') &&
-          e.innerHtml.contains('(') &&
-          e.innerHtml.contains('.') &&
-          !e.innerHtml.contains('<'));
-  for (final p in html) {
-    final raw = p.innerHtml
-        .replaceAll(RegExp('[ ­]'), '')
-        .replaceAll(RegExp('&.+?;'), '')
-        .split(',');
-    final fn = raw[1].split('.').first, ln = raw[0].split('.').last;
-    result['$ln $fn.'] = '$fn.$ln@gympeg.de'.toLowerCase();
+  try {
+    if (domain == null) return null;
+    final result = <String, String>{};
+
+    var html = htmlParse(
+      await httpGetFunc(Uri.parse('https://$domain/schulfamilie/lehrkraefte/')),
+    );
+    html = htmlSearchByClass(html, 'entry-content').children;
+    html = htmlSearchAllByPredicate(
+        html,
+        (e) =>
+            e.innerHtml.contains(',') &&
+            e.innerHtml.contains('(') &&
+            e.innerHtml.contains('.') &&
+            !e.innerHtml.contains('<'));
+
+    for (final p in html) {
+      final raw = p.innerHtml
+          .replaceAll(RegExp('[ ­]'), '')
+          .replaceAll(RegExp('&.+?;'), '')
+          .split(',');
+      final fn = raw[1].split('.').first, ln = raw[0].split('.').last;
+      result['$ln $fn.'] = '$fn.$ln@gympeg.de'.toLowerCase();
+    }
+
+    return result;
+  } catch (e) {
+    return null;
   }
-  return result;
+}
+
+Widget wpemailWidget(Map<String, String> emails) {
+  final w = <Widget>[];
+  for (final e in emails.entries)
+    w.add(ampListTile(e.key, subtitle: e.value, onTap: () async {
+      if (await canLaunch('mailto:${e.value}'))
+        await launch('mailto:${e.value}');
+    }));
+  return ampColumn(w);
 }
