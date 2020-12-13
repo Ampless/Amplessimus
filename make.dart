d21ca4d429f12b844ca8515a150323fa29e783b5
Chris Häußler (@ Windows) <chrissx@chrissx.de>
2020-12-13 09:14:10 +0100

    just - again - a better ci script

--- make.dart
+++ make.dart
@@ -58,25 +58,13 @@ Future mkdirs(d) => Directory(d).create(recursive: true);
 Future md5(path) => system("md5sum '$path' | cut -d' ' -f1");
 
 Future flutter(cmd) => system('flutter $cmd');
+Future build(cmd, flags) => flutter('build $cmd $flags');
+
 Future hdiutil(cmd) => system('hdiutil $cmd');
 Future strip(files) => system('strip -u -r $files');
 
-String sed(input, String regex, replacement) {
-  return input.toString().replaceAll(RegExp(regex), replacement.toString());
-}
-
-Future sedit(input, output, buildDir, {deb = '/dev/null'}) async {
-  var s = await readfile(input);
-  s = sed(s, '0.0.0-1', version);
-  s = sed(s, '\$ISIZE', (await Directory(buildDir).stat()).size);
-  s = sed(s, '\$SIZE', (await File(deb).stat()).size);
-  s = sed(s, '\$MD5', await md5(deb));
-  s = sed(s, '\$ARCH', 'iphoneos-arm');
-  await writefile(output, s);
-}
-
 Future iosapp(buildDir) async {
-  await flutter('build ios $iosFlags');
+  await build('ios', iosFlags);
   await system(
       'xcrun bitcode_strip $buildDir/Frameworks/Flutter.framework/Flutter -r -o tmpfltr');
   await mv('tmpfltr', '$buildDir/Frameworks/Flutter.framework/Flutter');
@@ -84,34 +72,20 @@ Future iosapp(buildDir) async {
   await strip('$buildDir/Runner $buildDir/Frameworks/*.framework/*');
 }
 
-Future ipa(buildDir, output) async {
+Future ipa(buildDir) async {
   //await flutter('build ipa $iosFlags');
   await system('cp -rp $buildDir tmp/Payload');
-  await rm(output);
-  await system('cd tmp && zip -r -9 ../$output Payload');
-}
-
-// http://www.saurik.com/id/7
-// but its broken...
-Future deb(buildDir, output) async {
-  await system('cp -rp $buildDir tmp/deb/Applications/');
-  await sedit('control.def', 'tmp/deb/DEBIAN/control', buildDir);
-  await system('COPYFILE_DISABLE= COPY_EXTENDED_ATTRIBUTES_DISABLE= '
-      'dpkg-deb -Sextreme -z9 --build tmp/deb $output');
-}
-
-Future cydiainfo(buildDir, output, debFile) async {
-  await sedit('Packages.def', '$output/Packages', buildDir, deb: debFile);
-  await system('gzip -9 -c $output/Packages > $output/Packages.gz');
+  await rm('bin/$version.ipa');
+  await system('cd tmp && zip -r -9 ../bin/$version.ipa Payload');
 }
 
 Future apk() async {
-  await flutter('build apk $apkFlags');
+  await build('apk', apkFlags);
   await mv('build/app/outputs/flutter-apk/app-release.apk', 'bin/$version.apk');
 }
 
 Future aab() async {
-  await flutter('build appbundle $aabFlags');
+  await build('appbundle', aabFlags);
   await mv(
       'build/app/outputs/bundle/release/app-release.aab', 'bin/$version.aab');
 }
@@ -124,8 +98,8 @@ Future test() async {
 
 Future ios() async {
   await iosapp('build/ios/Release-iphoneos/Runner.app');
-  await ipa('build/ios/Release-iphoneos/Runner.app', 'bin/$version.ipa');
-  await deb('build/ios/Release-iphoneos/Runner.app', 'bin/$version.deb');
+  await ipa('build/ios/Release-iphoneos/Runner.app');
+  //deb
 }
 
 Future android() async {
@@ -135,45 +109,44 @@ Future android() async {
 
 Future web() async {
   await flutter('config --enable-web');
-  await flutter('build web $webFlags');
+  await build('web', webFlags);
   await mvd('build/web', 'bin/$version.web');
 }
 
 Future win() async {
   await flutter('config --enable-windows-desktop');
-  await flutter('build windows $winFlags');
+  await build('windows', winFlags);
   await mvd('build/windows/runner/Release', 'bin/$version.win');
 }
 
 Future mac() async {
   await flutter('config --enable-macos-desktop');
-  await flutter('build macos $macFlags');
-  const build = 'build/macos/Build/Products/Release/Amplessimus.app';
-  const contents = '$build/Contents';
+  await build('macos', macFlags);
+  const bld = 'build/macos/Build/Products/Release/Amplessimus.app';
+  const contents = '$bld/Contents';
   await strip('$contents/Contents/Runner '
       '$contents/Contents/Frameworks/App.framework/Versions/A/App '
       '$contents/Contents/Frameworks/FlutterMacOS.framework/Versions/A/FlutterMacOS '
       '$contents/Contents/Frameworks/shared_preferences_macos.framework/Versions/A/shared_preferences_macos '
       '$contents/Contents/Frameworks/*.dylib');
 
-  await system('cp -rf $build tmp/dmg');
-  await system('ln -s /Applications $build/Applications');
-  await system(
-      'hdiutil create tmp/tmp.dmg -ov -srcfolder tmp/dmg -fs APFS -volname "Install Amplessimus"');
-  await system(
-      'hdiutil convert tmp/tmp.dmg -ov -format UDBZ -o bin/$version.dmg');
+  await system('cp -rf $bld tmp/dmg');
+  await system('ln -s /Applications $bld/Applications');
+  await hdiutil(
+      'create tmp/tmp.dmg -ov -srcfolder tmp/dmg -fs APFS -volname "Install Amplessimus"');
+  await hdiutil('convert tmp/tmp.dmg -ov -format UDBZ -o bin/$version.dmg');
 }
 
 Future linux() async {
   await flutter('config --enable-linux-desktop');
-  await flutter('build linux $gtkFlags');
+  await build('linux', gtkFlags);
   await mvd('build/linux/release/bundle', 'bin/$version.linux');
 }
 
 Future ci() async {
   await apk();
   await iosapp('build/ios/Release-iphoneos/Runner.app');
-  await ipa('build/ios/Release-iphoneos/Runner.app', 'bin/$version.ipa');
+  await ipa('build/ios/Release-iphoneos/Runner.app');
 }
 
 Future ver() async {
@@ -223,8 +196,7 @@ Future main(List<String> argv) async {
   } catch (e) {
     stderr.writeln(e);
     if (e is Error) stderr.writeln(e.stackTrace);
-  } finally {
-    await rmd('tmp');
-    await rmd('build');
   }
+  await rmd('tmp');
+  await rmd('build');
 }
