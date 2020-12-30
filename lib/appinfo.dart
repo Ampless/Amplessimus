d7082f33cbc13b814e536c814abcae349aea2335
Chris Häußler (@ Windows) <chrissx@chrissx.de>
2020-12-30 03:29:55 +0100

    things break on windows

--- lib/appinfo.dart
+++ lib/appinfo.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:package_info/package_info.dart';
 
 const String appTitle = 'Amplessimus';
-Future<String> get appVersion async =>
-    (await PackageInfo.fromPlatform()).version;
+Future<String> get appVersion async {
+  if (Platform.isWindows) return '0.0.0-1';
+  return (await PackageInfo.fromPlatform()).version;
+}
