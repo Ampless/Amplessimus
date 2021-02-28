bf725fe8052f8cc3e1b4b6974ca0ccf94a3cbfc5
chrissx <chrissx@chrissx.de>
2021-02-28 17:15:16 +0100

    updated some deps

--- lib/appinfo.dart
+++ lib/appinfo.dart
@@ -4,6 +4,8 @@ import 'package:package_info/package_info.dart';
 
 const String appTitle = 'Amplessimus';
 Future<String> get appVersion async {
+  //TODO: test if we have to do this (i need to get a windows for that)
   if (Platform.isWindows) return '0.0.0-1';
+  //TODO: make it x.y.z (num) in the app info
   return (await PackageInfo.fromPlatform()).version;
 }
